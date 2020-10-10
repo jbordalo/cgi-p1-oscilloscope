8f65af8051c039db26cc3c224f33dbcb7a5fb669
jbordalo <j.bordalo@campus.fct.unl.pt>
2020-10-10 16:54:25 +0100

    Add scaling functionality

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const TAN = 2;
 const HORIZONTAL_BLOCKS = 12;
 const VERTICAL_BLOCKS = 8;
 
+const STANDARD_SCALE = 1.0 / VERTICAL_BLOCKS * 2;
+
 /** @type {WebGLRenderingContext} */
 var gl;
 let currentFunction = SIN;
-let funcLoc, ampLoc, angFreqLoc, phaseLoc, colorLoc, colorLoc1; //TODO : color is Uniform
-let $selectFunc = document.getElementById('select-func');
-let currentAmp = 0.3, currentFreq = 2 * Math.PI, currentPhase = 2 * Math.PI;
+let funcLoc, ampLoc, angFreqLoc, phaseLoc, vScaleLoc, hScaleLoc, colorLoc, colorLoc1; //TODO : color is Uniform
+let currentAmp = 10.0 /* V */, currentFreq = 96 /* Hz */, currentPhase = 2 * Math.PI, currentHScale = 0.05, currentVScale = STANDARD_SCALE;
 
 let timeLoc;
 let time = 0;
@@ -21,12 +22,15 @@ var times;
 var bufferId;
 var bufferId1;
 
-
-let $amplitudeSlider = document.getElementById('amplitude-slider');
-$amplitudeSlider.oninput = () => {
-    console.log($amplitudeSlider.value);
-    currentAmp = $amplitudeSlider.value;
+function getScale(voltsPerBlock) {
+    return STANDARD_SCALE / voltsPerBlock;
 }
+
+let $verticalScaleSelector = document.getElementById('vertical-scale-selector');
+$verticalScaleSelector.addEventListener("change", e = () => {
+    console.log(parseFloat($verticalScaleSelector.value, 10));
+    currentVScale = getScale(parseFloat($verticalScaleSelector.options[$verticalScaleSelector.selectedIndex].value, 10));
+});
 
 let $frequencySlider = document.getElementById('frequency-slider');
 $frequencySlider.oninput = () => {
@@ -40,41 +44,7 @@ $phaseSlider.oninput = () => {
     currentPhase = $phaseSlider.value;
 };
 
-
-function genNumbers() {
-    let a = [];
-    for (let i = 0; i < 10000; i++) {
-        a.push(i);
-    }
-    return a;
-}
-
-function genGridPoints(vertical, horizontal) {
-    let v = [];
-    step = 2/horizontal;
-    for(let i = -1; i<1; i+= step){
-        v.push(vec2(1.0,i));
-        v.push(vec2(-1.0,i));
-    }
-    step = 2/vertical;
-    for(let i = -1; i<1; i+= step){
-        v.push(vec2(i, 1.0));
-        v.push(vec2(i,-1.0));
-    }
-    return v;
-}
-
-function toDrawData(program, bufferId, vector, attr, size){
-    gl.useProgram(program);
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vector), gl.STATIC_DRAW);
-    
-    var loc = gl.getAttribLocation(program, attr);
-    gl.vertexAttribPointer(loc, size, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(loc);   
-}
-
-
+let $selectFunc = document.getElementById('select-func');
 $selectFunc.addEventListener("change", e => {
     value = $selectFunc.options[$selectFunc.selectedIndex].value;
     console.log($selectFunc.options[$selectFunc.selectedIndex].value);
@@ -94,6 +64,39 @@ $selectFunc.addEventListener("change", e => {
     }
 });
 
+function genNumbers() {
+    let a = [];
+    for (let i = 0; i < 10000; i++) {
+        a.push(i);
+    }
+    return a;
+}
+
+function genGridPoints(vertical, horizontal) {
+    let v = [];
+    step = 2 / horizontal;
+    for (let i = -1; i < 1; i += step) {
+        v.push(vec2(1.0, i));
+        v.push(vec2(-1.0, i));
+    }
+    step = 2 / vertical;
+    for (let i = -1; i < 1; i += step) {
+        v.push(vec2(i, 1.0));
+        v.push(vec2(i, -1.0));
+    }
+    return v;
+}
+
+function toDrawData(program, bufferId, vector, attr, size) {
+    gl.useProgram(program);
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vector), gl.STATIC_DRAW);
+
+    var loc = gl.getAttribLocation(program, attr);
+    gl.vertexAttribPointer(loc, size, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(loc);
+}
+
 window.onload = function init() {
     var canvas = document.getElementById("gl-canvas");
     gl = WebGLUtils.setupWebGL(canvas);
@@ -106,19 +109,21 @@ window.onload = function init() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     // Load shaders and initialize attribute buffers
     program = initShaders(gl, "vertex-shader", "fragment-shader");
-    
+
     // Load the data into the GPU
     bufferId = gl.createBuffer();
     toDrawData(program, bufferId, times, "vTimeSample", 1);
-    
+
     funcLoc = gl.getUniformLocation(program, 'func');
     ampLoc = gl.getUniformLocation(program, 'amp');
     angFreqLoc = gl.getUniformLocation(program, 'angFreq');
     phaseLoc = gl.getUniformLocation(program, 'phase');
     colorLoc = gl.getUniformLocation(program, "vColor")
-    
+    vScaleLoc = gl.getUniformLocation(program, "vScale");
+    hScaleLoc = gl.getUniformLocation(program, "hScale");
+
     timeLoc = gl.getUniformLocation(program, 'time');
-    
+
     gridProgram = initShaders(gl, "grid-vertex-shader", "fragment-shader");
     bufferId1 = gl.createBuffer();
     grid = genGridPoints(HORIZONTAL_BLOCKS, VERTICAL_BLOCKS);
@@ -139,9 +144,11 @@ function render() {
     gl.uniform1f(angFreqLoc, 2 * Math.PI * currentFreq);
     gl.uniform1f(phaseLoc, currentPhase);
     gl.uniform1f(timeLoc, time);
+    gl.uniform1f(vScaleLoc, currentVScale);
+    gl.uniform1f(hScaleLoc, currentHScale)
     gl.uniform4fv(colorLoc, vec4(0.0, 1.0, 1.0, 1.0));
 
-    time += .007;
+    time += .00;
 
     gl.drawArrays(gl.LINE_STRIP, 0, 10000);
 
